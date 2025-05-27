a3fa1194b2fb7590c4447af20c1eed557c4013fe
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-05-27 13:50:35 -0300

    subindo estrutura básica do projeto

---
+++ src/test/java/com/naruto/demo/DesafioNarutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naruto.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNarutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
