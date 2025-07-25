bf5fee0792d000af3cc6c1a5c259a2bf5cecefb3
saulo.rodrigues <saulohrodrigues@gmail.com>
2025-07-25 11:39:49 -0300

    Retirando imports sem uso.

--- src/main/java/com/naruto/mappers/JutsuMapper.java
+++ src/main/java/com/naruto/mappers/JutsuMapper.java
@@ -1,7 +1,6 @@
 package com.naruto.mappers;
 
 import com.naruto.dto.jutsu.JutsuRequestDto;
-import com.naruto.dto.jutsu.JutsuResponseDto;
 import com.naruto.model.Jutsu;
 import org.mapstruct.Mapper;
 
